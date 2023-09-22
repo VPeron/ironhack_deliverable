07bd43df0b54986436e81e810c2efd248a2ef070
Vinicius Peron <viperon.python@gmail.com>
2023-09-22 15:39:12 +0200

    trying to set background image

--- homepage.py
+++ homepage.py
@@ -60,3 +60,27 @@ nuclear energy and critical-infrastruture.*
 <a href={role_of_cyber_url} target="_self"><button style="background-color:GreenYellow;">5. What is the role of cybersecurity</button></a>
   
 """, unsafe_allow_html=True)
+
+import base64
+
+
+# prep img format
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+# set background
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('static/powerplants_geodata2019.png')
