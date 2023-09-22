9c728750b3bb415367532332eb542716cb6b8b7f
Vinicius Peron <viperon.python@gmail.com>
2023-09-22 10:23:23 +0200

    first commit - homepage holder title and image

---
+++ crit_infra_attacks.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.title('Critical Infrastructures: Analyzing and Mitigating Cyber Threats in Nuclear Energy Plants (?)')
+
+st.image('https://www.worldatlas.com/r/w1200-q80/upload/3d/93/c7/shutterstock-1574089963.jpg')
