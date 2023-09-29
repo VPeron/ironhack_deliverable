400ed1d4f57fab147e8166c2535f4452ec0d79cd
Vinicius Peron <viperon.python@gmail.com>
2023-09-30 00:01:49 +0200

    add video example to history page

--- pages/3_history.py
+++ pages/3_history.py
@@ -9,6 +9,9 @@ setup_page_config()
 # content
 st.title("History of attacks")
 
+st.subheader('Stuxnet')
+
+
 # image slide
 carroussel_paths = [
     'static/types_of_powerplants.png',
@@ -17,6 +20,7 @@ carroussel_paths = [
 ]
 
 carrousel_build(carroussel_paths, 'hist_demo_path')
+st.video('https://www.youtube.com/watch?v=9DCwyuH29SI')
 
 hist_data = render_markdown('static/history.md')
 st.markdown(hist_data)
