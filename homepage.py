ca8b13a7a06e4baeb96584775d0493d081cf330a
Vinicius Peron <viperon.python@gmail.com>
2023-10-17 14:55:33 +0200

    splitting homepage content into columns

--- homepage.py
+++ homepage.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 from utils.img_bkground import set_background
+from utils.fetch_markdown import render_markdown
 
 
 st.set_page_config(
@@ -44,27 +45,40 @@ role_of_cyber_url = "https://ironhackdeliverable-group5.streamlit.app/the_role_o
 # content
 st.title('Guarding Critical Infrastructures: Analyzing and Mitigating Cyber Threats in Nuclear Energy Plants')
 
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown(f"""
+    
+    ### INDEX
+
+    <a href={definition_url} target="_self"><button style="background-color:GreenYellow;">1. Definition of Critical Infrastructure</button></a>
+
+    <a href={risks_and_threats_url} target="_self"><button style="background-color:GreenYellow;">2. Cyber Risks and Threats Relating to Critical Infrastructure</button></a>
+
+    <a href={history_url} target="_self"><button style="background-color:GreenYellow;">3. History of Attacks</button></a>
+
+    <a href={attack_vectors_url} target="_self"><button style="background-color:GreenYellow;">4. Modern Vectors of Attack</button></a>
+
+    <a href={role_of_cyber_url} target="_self"><button style="background-color:GreenYellow;">5. What is The Role of Cybersecurity Within Critical-Infrastructure targets</button></a>
+
+
+    """, unsafe_allow_html=True)
+
+with col2:
+    st.markdown(f"""
+    ### Group Members:
+    - Antonio
+    - Cesar
+    - Rabia
+    - Vini  
+    """, unsafe_allow_html=True)
+
+intro_text = render_markdown('static/Introduction.md')
 st.markdown(f"""
-### Group Members:
-- Antonio
-- Cesar
-- Rabia
-- Vini  
-
-### Our bootcamp capstone-project aims to illustrate the importance of CyberSecurity in the realms of nuclear energy as critical-infrastruture targets.
-
 ---
-
-### INDEX
-
-<a href={definition_url} target="_self"><button style="background-color:GreenYellow;">1. Definition of Critical Infrastructure</button></a>
-
-<a href={risks_and_threats_url} target="_self"><button style="background-color:GreenYellow;">2. Cyber Risks and Threats Relating to Critical Infrastructure</button></a>
-
-<a href={history_url} target="_self"><button style="background-color:GreenYellow;">3. History of Attacks</button></a>
-
-<a href={attack_vectors_url} target="_self"><button style="background-color:GreenYellow;">4. Modern Vectors of Attack</button></a>
-
-<a href={role_of_cyber_url} target="_self"><button style="background-color:GreenYellow;">5. What is The Role of Cybersecurity Within Critical-Infrastructure targets</button></a>
-  
+{intro_text}
 """, unsafe_allow_html=True)
+
+
+
