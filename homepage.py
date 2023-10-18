803ebd19f9f0f537b1558494b312b0b9c0f64931
Vinicius Peron <viperon.python@gmail.com>
2023-10-18 12:10:20 +0200

    improved text readability with bold chars and changed home background img to lighter version overall

--- homepage.py
+++ homepage.py
@@ -33,7 +33,7 @@ set_custom_footer = """
 st.markdown(set_custom_footer, unsafe_allow_html=True)
 
 # set background img
-set_background('static/home/nuclear_plant_leonardo.jpg')
+set_background('static/home/nuclear_background_light.jpg')
 
 # assign pages url paths
 definition_url = "https://ironhackdeliverable-group5.streamlit.app/definition_of_critical_infrastructure"
@@ -52,15 +52,15 @@ with col1:
     
     ### INDEX
 
-    <a href={definition_url} target="_self"><button style="background-color:GreenYellow;">1. Definition of Critical Infrastructure</button></a>
+    <a href={definition_url} target="_self"><button style="background-color:White;">1. Definition of Critical Infrastructure</button></a>
 
-    <a href={risks_and_threats_url} target="_self"><button style="background-color:GreenYellow;">2. Cyber Risks and Threats Relating to Critical Infrastructure</button></a>
+    <a href={risks_and_threats_url} target="_self"><button style="background-color:White;">2. Cyber Risks and Threats Relating to Critical Infrastructure</button></a>
 
-    <a href={history_url} target="_self"><button style="background-color:GreenYellow;">3. History of Attacks</button></a>
+    <a href={history_url} target="_self"><button style="background-color:White;">3. History of Attacks</button></a>
 
-    <a href={attack_vectors_url} target="_self"><button style="background-color:GreenYellow;">4. Modern Vectors of Attack</button></a>
+    <a href={attack_vectors_url} target="_self"><button style="background-color:White;">4. Modern Vectors of Attack</button></a>
 
-    <a href={role_of_cyber_url} target="_self"><button style="background-color:GreenYellow;">5. What is The Role of Cybersecurity Within Critical-Infrastructure targets</button></a>
+    <a href={role_of_cyber_url} target="_self"><button style="background-color:White;">5. What is The Role of Cybersecurity Within Critical-Infrastructure targets</button></a>
 
 
     """, unsafe_allow_html=True)
@@ -68,10 +68,10 @@ with col1:
 with col2:
     st.markdown(f"""
     ### Group Members:
-    - Antonio
-    - Cesar
-    - Rabia
-    - Vini  
+    - **Antonio**
+    - **Cesar**
+    - **Rabia**
+    - **Vini**  
     """, unsafe_allow_html=True)
 
 intro_text = render_markdown('static/home/Introduction.md')
